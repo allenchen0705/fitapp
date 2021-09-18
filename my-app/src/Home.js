b179f11a6e7657f6c88cff33ba114ca84d53db22
sarahhjchung <sarahc0304@gmail.com>
2021-09-18 17:34:12 -0400

    implement basic frontend for home

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -4,13 +4,20 @@ import { ReactComponent as Logo } from "./assets/logo.svg";
 function Home() {
   return (
     <body>
-      <header className="container">
-        <Logo className="logo"/>
+      <header className="container navbar">
+        <a href="/"><Logo className="logo"/></a>
+        <a href="/" className="explore">Explore</a>
+        <a href="/create" className="create">+ Create a Post</a>
       </header>
-      <div className="Home">
-        This is the Home.
-        <a href="/create">Create Post</a>
+      <div className="title">
+        <h2>Get inspired. Learn fitness</h2>
+        <p>Explore all fitness posts from others around the world</p>
       </div>
+      <section className="grid">
+        <div className="search-bar"><p>Search ...</p></div>
+        <div className="categories"><p>Categories</p></div>
+        <div className="sort"><p>Sort by</p></div>
+      </section>
     </body>
     
   );
