ca47a351af5a39b391e91d258d341a2c9fd64500
Sicong Chen <allennn.chen@mail.utoronto.ca>
2021-09-18 18:07:10 -0400

    create post page

--- my-app/src/CreatePost.js
+++ my-app/src/CreatePost.js
@@ -1,10 +1,18 @@
-// import './CreatePost.css';
+import './CreatePost.css';
+import { ReactComponent as Logo } from "./assets/logo.svg";
 
 function CreatePost() {
   return (
     <div className="CreatePost">
-      This is CreatePost.
-      <a href="/thankyou">Thank you</a>
+      <Logo className="logo" />
+      <div className="title">
+        <h1 className="inspire">Inspire others with your experiences</h1>
+        <h3 className="choose">Choose a category of posts and use our templates to share your fitness experience with others.</h3>
+        <h2 className="theme">What is the main theme of your experience?</h2>
+        <a className="post" href="/thankyou">Post!</a>
+      </div>
+
+
     </div>
   );
 }
