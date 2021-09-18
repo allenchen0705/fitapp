b0ec316c859c078cf4058ee784159d8a8ab8fffa
sarahhjchung <sarahc0304@gmail.com>
2021-09-18 15:04:25 -0400

    added bg image and logo

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,11 +1,18 @@
-// import './Home.css';
+import './Home.css';
+import { ReactComponent as Logo } from "./assets/logo.svg";
 
 function Home() {
   return (
-    <div className="Home">
-      This is the Home.
-      <a href="/create">Create Post</a>
-    </div>
+    <body>
+      <header className="container">
+        <Logo className="logo"/>
+      </header>
+      <div className="Home">
+        This is the Home.
+        <a href="/create">Create Post</a>
+      </div>
+    </body>
+    
   );
 }
 
