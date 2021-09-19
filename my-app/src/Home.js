258889cd09ddbe4f3ae11820764716effa4ad628
sarahhjchung <sarahc0304@gmail.com>
2021-09-19 01:54:04 -0400

    added temporary blog posts

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,5 +1,6 @@
 import "./Home.css";
 import { ReactComponent as Logo } from "./assets/logo.svg";
+import BlogPost from "./BlogPost";
 
 function Home() {
   return (
@@ -26,6 +27,50 @@ function Home() {
         <div className="sort dropdown">
           <p>Sort by</p>
         </div>
+      </section>
+      <section>
+        <BlogPost
+          title="Keto Diet"
+          author="Deer John"
+          category="Food and Nutrition"
+          calories={true}
+          caloriesName="+300"
+          diet={true}
+          dietName="Keto Diet"
+          blurb="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas maecenas pharetra convallis posuere. Tellus id interdum velit laoreet id donec ultrices tincidunt arcu. Tincidunt eget nullam non nisi."
+        />
+        <br />
+        <br />
+        <BlogPost
+          title="Badminton Drills"
+          author="Sara Chong"
+          category="Sports Training"
+          calories={false}
+          caloriesName=""
+          diet={false}
+          dietName=""
+          sport={true}
+          sportName="Badminton"
+          blurb="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas maecenas pharetra convallis posuere. Tellus id interdum velit laoreet id donec ultrices tincidunt arcu. Tincidunt eget nullam non nisi."
+        />
+        <br />
+        <br />
+        <BlogPost
+          title="Leg Day for Days"
+          author="Alen Chan"
+          category="Strength Training"
+          calories={false}
+          caloriesName=""
+          diet={false}
+          dietName=""
+          time={true}
+          timeName="2 hours"
+          strength={true}
+          strengthName="Leg Training"
+          blurb="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas maecenas pharetra convallis posuere. Tellus id interdum velit laoreet id donec ultrices tincidunt arcu. Tincidunt eget nullam non nisi."
+        />
+        <br />
+        <br />
       </section>
     </body>
   );
